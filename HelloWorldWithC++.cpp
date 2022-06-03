bd82a8b465b65b9309315f751e18ab2aaca3adf6
M. Samiul Hasnat <58338543+samiulhsnt@users.noreply.github.com>
2022-06-03 19:54:54 +0600

    1st problem

---
+++ HelloWorldWithC++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
+
+//Problem Link
+/*
+https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+*/
