bc45c8d8d95658274904dc981edaf9c4676df67e
M. Samiul Hasnat <58338543+samiulhsnt@users.noreply.github.com>
2022-06-03 19:56:06 +0600

    2nd Problem

---
+++ InputOutput.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b,c, sum;
+    
+    cin >> a >> b>> c;
+    sum = a+b+c;
+    cout << sum;
+    
+    return 0;
+}
+
+// Problem Link
+/*
+	https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+*/
